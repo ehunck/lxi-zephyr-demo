598875d6b08a39b700b2acea10676eb8714c046f
ehunck <ehunck@aol.com>
2025-02-12 23:59:59 -0600

    - add in additional png to http server

--- app/src/http_server.c
+++ app/src/http_server.c
@@ -27,16 +27,20 @@ static struct http_resource_detail_static index_html_gz_resource_detail = {
 	.static_data_len = sizeof(index_html_gz),
 };
 
-// static struct http_resource_detail_static main_js_gz_resource_detail = {
-// 	.common = {
-// 			.type = HTTP_RESOURCE_TYPE_STATIC,
-// 			.bitmask_of_supported_http_methods = BIT(HTTP_GET),
-// 			.content_encoding = "none",
-// 			.content_type = "text/html",
-// 		},
-// 	.static_data = main_js_gz,
-// 	.static_data_len = sizeof(main_js_gz),
-// };
+static const uint8_t millipede_logo_html_gz[] = {
+    #include "millipede-logo-dark-gray.png.gz.inc"
+};
+
+static struct http_resource_detail_static millipede_logo_html_gz_resource_detail = {
+	.common = {
+			.type = HTTP_RESOURCE_TYPE_STATIC,
+			.bitmask_of_supported_http_methods = BIT(HTTP_GET),
+			.content_encoding = "gzip",
+			.content_type = "text/html",
+		},
+	.static_data = millipede_logo_html_gz,
+	.static_data_len = sizeof(millipede_logo_html_gz),
+};
 
 static uint16_t http_service_port = 80;
 
@@ -45,7 +49,6 @@ HTTP_SERVICE_DEFINE(test_http_service, "0.0.0.0", &http_service_port, 1, 10, NUL
 HTTP_RESOURCE_DEFINE(index_html_gz_resource, test_http_service, "/",
 		     &index_html_gz_resource_detail);
 
-// HTTP_RESOURCE_DEFINE(main_js_gz_resource, test_http_service, "/configure",
-// 		     &main_js_gz_resource_detail);
-
+HTTP_RESOURCE_DEFINE(millipede_logo_html_gz_resource, test_http_service, "/millipede-logo-dark-gray.png",
+		     &millipede_logo_html_gz_resource_detail);
 
